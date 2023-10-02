ddecb56da9a650bff218d431ba0d9554c1b74640
reflexson <131407662+reflexson@users.noreply.github.com>
2023-10-02 14:17:37 -0500

    final

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,17 +4,28 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
   event.preventDefault();
+      // Store the triggered events
+      window.deferredPrompt = event;
   butInstall.style.visibility = 'visible';
   butInstall.textContent = 'Install'
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
+  const promptEvent = window.deferredPrompt;
+  if (!promptEvent) {
+   return;
+  }
+  // Show prompt
+  promptEvent.prompt();
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
   butInstall.setAttribute('disabled', true);
   butInstall.textContent = 'Installed';
   });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
+  window.deferredPrompt = null;
   console.log('Appinstalled', event);
 });
